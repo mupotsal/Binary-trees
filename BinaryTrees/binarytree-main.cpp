66e2a9a221883b3bd8836963720dd67f8ea85231
Anthony Wafula <wafulaa@berea.edu>
2019-11-15 22:02:20 -0500

    Added anme and lab requirments

--- BinaryTrees/binarytree-main.cpp
+++ BinaryTrees/binarytree-main.cpp
@@ -1,5 +1,9 @@
 /* Binary Tree guessing game
 FIXME: Expand this with names and purpose!
+NAME: ANTHONY W WAFULA
+CLASS: CSC 236
+REQUIREMENTS: Ask the user to gues animals acording to the value in the text file 
+          answers picked are yes or false as they try to figure out the animal in there mind
 */
 
 #include <iostream>
