9e0308ae0ce05194da577d07d818ef0723bf9fcd
Jan Pearce <pearcej@gmail.com>
2019-11-15 21:59:06 -0500

    Initial commit

---
+++ BinaryTrees/binarytree-main.cpp
@@ -0,0 +1,20 @@
+/* Binary Tree guessing game
+FIXME: Expand this with names and purpose!
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char readchar;
+
+	//FIXME: Code needed here
+
+
+
+
+
+	cin >> readchar; //This is to keep screen open in some situations.
+	return 0;
+}
