64aa236d5cca072e6dca492cf33f01098a0665a0
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-11-17 17:02:20 -0500

    Added name and clonned the repository

--- BinaryTrees/binarytree-main.cpp
+++ BinaryTrees/binarytree-main.cpp
@@ -1,6 +1,8 @@
+
 /* Binary Tree guessing game
 FIXME: Expand this with names and purpose!
-NAME: ANTHONY W WAFULA
+NAMES: ANTHONY W WAFULA &LIBERTY MUPOTSA
+
 CLASS: CSC 236
 REQUIREMENTS: Ask the user to gues animals acording to the value in the text file 
           answers picked are yes or false as they try to figure out the animal in there mind
