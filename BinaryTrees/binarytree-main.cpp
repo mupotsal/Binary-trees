f19a973cf5920952b63cb65121ddf6a0b2b4e6a2
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-11-23 22:23:49 -0500

    Attempted

--- BinaryTrees/binarytree-main.cpp
+++ BinaryTrees/binarytree-main.cpp
@@ -2,7 +2,6 @@
 /* Binary Tree guessing game
 FIXME: Expand this with names and purpose!
 NAMES: ANTHONY W WAFULA &LIBERTY MUPOTSA
-
 CLASS: CSC 236
 REQUIREMENTS: Ask the user to gues animals acording to the value in the text file 
           answers picked are yes or false as they try to figure out the animal in there mind
@@ -11,13 +10,74 @@ REQUIREMENTS: Ask the user to gues animals acording to the value in the text fil
 #include <iostream>
 using namespace std;
 
+class BinaryTree{
+private:
+	char key;
+	BinaryTree* leftChild;
+	BinaryTree* rightChild;
+public:
+	BinaryTree(char rootObj) {
+		this->key = rootObj;
+		this->leftChild = NULL;
+		this->rightChild = NULL;
+	}
+
+	void insertLeft(char newNode) {
+		if (this->leftChild == NULL) {
+			this->leftChild = new BinaryTree(newNode);
+		}
+		else {
+			BinaryTree* t = new BinaryTree(newNode);
+			t->leftChild = this->leftChild;
+			this->leftChild = t;
+		}
+	}
+
+	void insertRight(char newNode) {
+		if (this->rightChild == NULL) {
+			this->rightChild = new BinaryTree(newNode);
+		}
+		else {
+			BinaryTree* t = new BinaryTree(newNode);
+			t->rightChild = this->rightChild;
+			this->rightChild = t;
+		}
+	}
+
+	BinaryTree* getRightChild() {
+		return this->rightChild;
+	}
+
+	BinaryTree* getLeftChild() {
+		return this->leftChild;
+	}
+
+	void setRootVal(char obj) {
+		this->key = obj;
+	}
+
+	char getRootVal() {
+		return this->key;
+	}
+
+
+	   	 
+};
+
+
+
+
+
+
+
+
 int main() {
 
 	char readchar;
 
 	//FIXME: Code needed here
 
-
+	// we need to find out what we are reading from the files
 
 
 
