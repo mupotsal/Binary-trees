cd95ee3a94a08f335a2d09abfaa6214f45c87b62
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-11-24 21:47:52 -0500

    edited the function

--- BinaryTrees/binarytree-main.cpp
+++ BinaryTrees/binarytree-main.cpp
@@ -4,7 +4,7 @@ FIXME: Expand this with names and purpose!
 NAMES: ANTHONY W WAFULA &LIBERTY MUPOTSA
 CLASS: CSC 236
 REQUIREMENTS: Ask the user to gues animals acording to the value in the text file 
-          answers picked are yes or false as they try to figure out the animal in there mind
+answers picked are yes or false as they try to figure out the animal in there mind
 */
 
 #include <iostream>
@@ -51,7 +51,7 @@ public:
 	}
 
 
-	 bool isQuestion()	{
+	 bool isQuestion()	{ // is leaf
 		 if (noData == NULL && yesData == NULL) {
 			 return false;
 		 }
@@ -60,7 +60,7 @@ public:
 		 }
 	}
 
-	 void traverse() {
+	 void traverse() { // query
 		 if (this->isQuestion()) {
 			 cout << this->Question;
 			 string answer;
@@ -184,6 +184,7 @@ void newGame() {
 
 
 }
+
 
 
 
