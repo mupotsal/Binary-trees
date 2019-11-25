1e827b4726cb5845e1d4c15649776ab483c532d9
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-11-24 21:30:23 -0500

    Tried to make the Animal Guessing game

--- BinaryTrees/binarytree-main.cpp
+++ BinaryTrees/binarytree-main.cpp
@@ -8,21 +8,219 @@ REQUIREMENTS: Ask the user to gues animals acording to the value in the text fil
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
+#include <fstream>
 
-class BinaryTree{
+
+class Node {
 private:
-	char key;
+	string Question;
+	Node *noData;
+	Node* yesData;
+
+public:
+	Node(string quest) {
+		Question = quest;
+		noData = NULL;
+		yesData = NULL;
+	}
+	void setQuestion(string newQuestion)	{
+		Question = newQuestion;
+
+	}
+	string getQuestion()	{
+		return Question;
+
+	}
+	void setNoData(Node *node)
+	{
+		noData= node;
+	}
+	Node *getNoData()
+	{
+		return noData;
+	}
+	void setYesData(Node *node)
+	{
+		yesData= node;
+	}
+	Node *getYesNode()
+	{
+		return yesData;
+	}
+
+
+	 bool isQuestion()	{
+		 if (noData == NULL && yesData == NULL) {
+			 return false;
+		 }
+		 else {
+			 return true;
+		 }
+	}
+
+	 void traverse() {
+		 if (this->isQuestion()) {
+			 cout << this->Question;
+			 string answer;
+			 cout << "Enter yes or no" << endl;
+			 cin >> answer;
+			 if (answer == "yes") {
+				 yesData->traverse();
+			 }
+			 else {
+				 noData->traverse();
+			 }
+			 			 					  
+		 }
+
+		 else {
+			 this->onTraverseObject();
+		 }
+	 }
+
+
+	 void onTraverseObject() {
+		 cout << "I guess you are thinking of" << this->Question << "?" << endl;
+		 string answer;
+		 cout << "enter yes or no" << endl;
+		 if (answer == "yes") {
+			 cout << "The Computer Wins" << endl;
+		 }
+		 else {
+			 updateTree();
+		 }
+	 }
+
+	 void updateTree() {
+		 cout << "You won, what animal were you thinking of?";
+		 string useranimal;
+		 string userqsn;
+		 cout << "Write a question to diistinguish " << useranimal << " to " << this->Question << endl;
+		 cin >> userqsn;
+		 cout << "What would be the answer if you were thinking  of " << useranimal << endl;
+		 string answer;
+		 cin >>answer;
+		 if (answer == "yes") {
+			 this->noData = new Node(this->Question);
+			 this->yesData = new Node(useranimal);
+		 }
+		 else {
+			 this->yesData = new Node(this->Question);
+			 this->noData = new Node(useranimal);
+		 }
+
+
+	 }
+
+	
+	 
+};
+
+
+
+
+
+
+class BTTree {
+private:
+	Node* rootNode;
+public:
+	BTTree(string question, string yes_guess, string no_guess) {
+		rootNode = new Node(question);
+		rootNode->setYesData(new Node(yes_guess));
+		rootNode->setNoData(new Node(no_guess));
+	}
+
+	void query() {
+		rootNode->traverse();
+	}
+
+	void readfile(string fileName) {
+		string line;
+		ifstream readFile(fileName);
+		int lineCount = 0;
+		if (!readFile.is_open()) {// Checking for errors opening the file
+			cout << ("Error opening file.");
+			return;
+		}
+		while (getline(readFile, line)) {// Reading file until the end is reached or an error occurs
+			Node tree();
+			for (unsigned int i = 0; i < line.length(); ++i) {
+				// thisLine.push_back(line[i]);
+				if (line[i] == 'Q') {
+					//this->
+					cout << "What is this" << endl;
+
+				}
+			}
+
+		}
+		if (readFile.bad()) { // More error checking
+			cout << ("Error reading file.");
+			return;
+		}
+		readFile.close();
+
+		return;
+	}
+};
+
+
+void newGame() {
+
+	string question;
+	string yesGuess;
+	string noGuess;
+	cout << "what is the queston" << endl;
+	cin >> question;
+	cout << "what is the answer" << endl;
+	cin >> yesGuess;
+	cout << "what is the no answer" << endl;
+	cin >> noGuess;
+	BTTree tree(question, yesGuess, noGuess);
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class BinaryTree{
+private:
+	string guess;
 	BinaryTree* leftChild;
 	BinaryTree* rightChild;
 public:
-	BinaryTree(char rootObj) {
-		this->key = rootObj;
+	BinaryTree(string rootObj) {
+		this->guess = rootObj;
 		this->leftChild = NULL;
 		this->rightChild = NULL;
 	}
 
-	void insertLeft(char newNode) {
+	void insertLeft(string  newNode) {
 		if (this->leftChild == NULL) {
 			this->leftChild = new BinaryTree(newNode);
 		}
@@ -33,7 +231,7 @@ public:
 		}
 	}
 
-	void insertRight(char newNode) {
+	void insertRight(string newNode) {
 		if (this->rightChild == NULL) {
 			this->rightChild = new BinaryTree(newNode);
 		}
@@ -52,19 +250,20 @@ public:
 		return this->leftChild;
 	}
 
-	void setRootVal(char obj) {
-		this->key = obj;
+	void setRootVal(string obj) {
+		this->guess = obj;
 	}
 
-	char getRootVal() {
-		return this->key;
+	string getRootVal() {
+		return this->guess;
 	}
 
+	void replace(string qsn, string = "yes", string = "no") {
 
-	   	 
+	}
+
 };
-
-
+*/
 
 
 
@@ -73,14 +272,27 @@ public:
 
 int main() {
 
-	char readchar;
+	string question;
+	string yesGuess;
+	string noGuess;
+	cout << "what is the queston" << endl;
+	cin >> question;
+	cout << "what is the answer" << endl;
+	cin >> yesGuess;
+	//cout << "what is the no answer" << endl;
+	//gcin >> noGuess;
+	BTTree tree(question, yesGuess, noGuess);
+	tree.query();
 
-	//FIXME: Code needed here
+	while (true) {
+		BTTree tree(question, yesGuess, noGuess);
+		tree.query();
 
-	// we need to find out what we are reading from the files
+	}
+	ifstream inFile("animalguessing.txt");
+	tree.readfile("animalguessing.txt");
+	
+	
 
-
-
-	cin >> readchar; //This is to keep screen open in some situations.
 	return 0;
 }
